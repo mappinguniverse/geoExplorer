07b76430b355502e01dbe4944fbd060b8adbfebc
srfalessio <srf.alessio@gmail.com>
2020-11-17 09:52:29 +0100

    Add download static map

--- geoExploreR/inst/app/shapeExploreR/server.R
+++ geoExploreR/inst/app/shapeExploreR/server.R
@@ -1,161 +1,132 @@
-shinyServer(function(input, output, session)  {
-    options(shiny.maxRequestSize=50*1024^2)
+shinyServer(function(input, output, session) {
+  options(shiny.maxRequestSize = 50 * 1024^2)
 
-    data <- reactive({
-        req(input$file)
-        shp <- input$file
-        if(nrow(shp) == 1)
-        {
-            shp_dir <- input$file$datapath
+  data <- reactive({
+    req(input$file)
+    shp <- input$file
+    if (nrow(shp) == 1) {
+      shp_dir <- input$file$datapath
+    } else {
+      dir_prv <- getwd()
+      uploadDirectory <- dirname(shp$datapath[1])
+      setwd(uploadDirectory)
+      for (i in 1:nrow(shp)) {
+        file.rename(shp$datapath[i], shp$name[i])
+      }
+      shp_names <- shp$name[grep(x = shp$name, pattern = "*.shp")]
+      shp_dir <- paste(uploadDirectory, shp_names, sep = "/")
+      setwd(dir_prv)
+    }
 
-        }else
-        {
-            dir_prv <- getwd()
-            uploadDirectory <- dirname(shp$datapath[1])
-            setwd(uploadDirectory)
-            for (i in 1:nrow(shp)){
-                file.rename(shp$datapath[i], shp$name[i])
-            }
-            shp_names <- shp$name[grep(x=shp$name, pattern="*.shp")]
-            shp_dir <- paste(uploadDirectory, shp_names, sep="/")
-            setwd(dir_prv)
-        }
+    df <- read_sf(shp_dir)
+    dt$df <- df
+  })
 
-        df <- read_sf(shp_dir)
-        dt$df <- df
-    })
+  dt <- reactiveValues()
 
-    dt <- reactiveValues()
+  output$table <- renderDataTable({
+    req(input$file)
+    tb <- data()
+    return(tb[, -ncol(tb), drop = TRUE])
+  })
 
-    output$table <- renderDataTable({
-        req(input$file)
-        tb = data()
-        return(tb[,-ncol(tb), drop = TRUE])
-        })
+  output$map <- renderLeaflet({
+    tmap_mode("view")
+    map <- data()
 
-    output$map <- renderLeaflet({
-
-            tmap_mode("view")
-            map = data()
-
-            if(input$variable == " " | input$variable == "NONE")
-            {
-                ciao <- tm_shape(map) + tm_polygons(col = 'grey80', popup.vars = input$pop) + tm_tiles(input$tiles)
-
-            }else{
-                ciao <- tm_shape(map) + tm_polygons(col = input$variable, style = input$legend, palette = input$palette, popup.vars = input$pop)
-
-            }
-            return(tmap_leaflet(ciao))
-
-    })
+    if (input$variable == " " | input$variable == "NONE") {
+      ciao <- tm_shape(map) + tm_polygons(col = "grey80", popup.vars = input$pop) + tm_tiles(input$tiles)
+    } else {
+      ciao <- tm_shape(map) + tm_polygons(col = input$variable, style = input$legend, palette = input$palette, popup.vars = input$pop)
+    }
+    return(tmap_leaflet(ciao))
+  })
 
 
-    observe({
-        tb = data()
-        tb <- tb[,-ncol(tb), drop = TRUE]
+  observe({
+    tb <- data()
+    tb <- tb[, -ncol(tb), drop = TRUE]
 
-        updateSelectInput(session, inputId="variables", label="Variables",
-                          choices=names(tb), selected = names(tb))
-        updateSelectInput(session, inputId="variable", label="Color variable",
-                          choices=c(names(tb),"NONE"), selected = " ")
-        updateSelectInput(session, inputId="pop", label="Popup Variables",
-                           choices= names(tb), selected = names(tb))
-
-        output$table <- renderDataTable({
-            return(tb[,input$variables, drop = FALSE])
-        })
-
-    })
-
-    output$download <- downloadHandler(
-        filename = function() {
-
-            if(input$download_type == "data.csv")
-            {
-                paste("data", "csv", sep = ".")
-
-            }else if(input$download_type == "data.RData")
-            {
-               paste("data", "RData", sep = ".")
-            }else if(input$download_type == "data.geojson")
-            {
-                paste("data", "geojson", sep = ".")
-
-            }else if(input$download_type == "map&data.RData")
-            {
-                paste("dataMap", "Rdata", sep = ".")
-            }else if (input$download_type == "map.pdf")
-            {
-                paste("map", "pdf", sep = ".")
-
-            }else if(input$download_type == "map.jpeg")
-            {
-                paste("map", "jpeg", sep = ".")
-            }else if(input$download_type == "map.png")
-            {
-                paste("map", "png", sep = ".")
-            }else if(input$download_type == "map.html")
-            {
-                paste("map", "html", sep = ".")
-            }
-
-        },
-        content = function(file)
-        {
-            dt <- data()
-            df <- dt[,-ncol(dt), drop = TRUE]
-
-            if(input$download_type == "data.csv")
-            {
-                write.csv(df, file, row.names = FALSE)
-
-            }else if(input$download_type == "data.RData")
-            {
-                save(df, file = file)
-            }else if(input$download_type == "data.geojson")
-            {
-                geojson_write(input = dt,
-                              geometry = "polygon", file = file,
-                              crs = "+init=epsg:4326")
-
-            }else if(input$download_type == "map&data.RData")
-            {
-                save(df, dt, file = file)
-            }else{
-
-                tmap_mode("view")
-                map = data()
-
-                if(input$variable == " " | input$variable == "NONE")
-                {
-                    ciao <- tm_shape(map) + tm_polygons(col = 'grey80', popup.vars = input$pop) + tm_tiles(input$tiles)
-
-                }else{
-                    ciao <- tm_shape(map) + tm_polygons(col = input$variable, style = input$legend, palette = input$palette, popup.vars = input$pop)
-
-                }
-
-                if(input$download_type == "map.pdf")
-                {
-                mapshot(tmap_leaflet(ciao), file = file)
-                }else if(input$download_type == "map.jpeg")
-                {
-                    mapshot(tmap_leaflet(ciao), file = file)
-
-                }else if(input$download_type == "map.png")
-                {
-                    mapshot(tmap_leaflet(ciao), file = file)
-
-                }else if(input$download_type == "map.html")
-                {
-                    mapshot(tmap_leaflet(ciao), url = file)
-
-                }
-            }
-        }
-
+    updateSelectInput(session,
+      inputId = "variables", label = "Variables",
+      choices = names(tb), selected = names(tb)
+    )
+    updateSelectInput(session,
+      inputId = "variable", label = "Color variable",
+      choices = c(names(tb), "NONE"), selected = " "
+    )
+    updateSelectInput(session,
+      inputId = "pop", label = "Popup Variables",
+      choices = names(tb), selected = names(tb)
     )
 
-})
+    output$table <- renderDataTable({
+      return(tb[, input$variables, drop = FALSE])
+    })
+  })
 
+  output$download <- downloadHandler(
+    filename = function() {
+      if (input$download_type == "data.csv") {
+        paste("data", "csv", sep = ".")
+      } else if (input$download_type == "data.RData") {
+        paste("data", "RData", sep = ".")
+      } else if (input$download_type == "data.geojson") {
+        paste("data", "geojson", sep = ".")
+      } else if (input$download_type == "map&data.RData") {
+        paste("dataMap", "Rdata", sep = ".")
+      } else if (input$download_type == "map.pdf") {
+        paste("map", "pdf", sep = ".")
+      } else if (input$download_type == "map.jpeg") {
+        paste("map", "jpeg", sep = ".")
+      } else if (input$download_type == "map.png") {
+        paste("map", "png", sep = ".")
+      } else if (input$download_type == "map.html") {
+        paste("map", "html", sep = ".")
+      } else if (input$download_type == "map_static.pdf") {
+        paste("map", "pdf", sep = ".")
+      } else if (input$download_type == "map_static.jpeg") {
+        paste("map", "jpeg", sep = ".")
+      } else if (input$download_type == "map_static.png") {
+        paste("map", "png", sep = ".")
+      }
+    },
+    content = function(file) {
+      dt <- data()
+      df <- dt[, -ncol(dt), drop = TRUE]
+
+      if (input$download_type == "data.csv") {
+        write.csv(df, file, row.names = FALSE)
+      } else if (input$download_type == "data.RData") {
+        save(df, file = file)
+      } else if (input$download_type == "data.geojson") {
+        geojson_write(
+          input = dt,
+          geometry = "polygon", file = file,
+          crs = "+init=epsg:4326"
+        )
+      } else if (input$download_type == "map&data.RData") {
+        save(df, dt, file = file)
+      } else {
+        if (substr(input$download_type, 4, 4) == "_") {
+          tmap_mode("plot")
+        } else {
+          tmap_mode("view")
+        }
+        map <- data()
+
+        if (input$variable == " " | input$variable == "NONE") {
+          ciao <- tm_shape(map) + tm_polygons(col = "grey80", popup.vars = input$pop) + tm_tiles(input$tiles)
+        } else {
+          ciao <- tm_shape(map) + tm_polygons(col = input$variable, style = input$legend, palette = input$palette, popup.vars = input$pop)
+        }
+
+        if (input$download_type == "map.pdf" | input$download_type == "map.jpeg" | input$download_type == "map.png" | input$download_type == "map.html") {
+          mapshot(tmap_leaflet(ciao), file = file)
+        } else if (input$download_type == "map_static.pdf" | input$download_type == "map_static.jpeg" | input$download_type == "map_static.png") {
+          tmap_save(ciao, filename = file)
+        }
+      }
+    }
+  )
+})
--- geoExploreR/inst/app/shapeExploreR/ui.R
+++ geoExploreR/inst/app/shapeExploreR/ui.R
@@ -1,21 +1,21 @@
-ui <- dashboardPage(skin = "blue",
+ui <- dashboardPage(
+  skin = "blue",
   dashboardHeader(title = "geoExplorer", disable = FALSE),
   dashboardSidebar(
     sidebarMenu(
       tags$head(tags$style(".butt{background-color:white;} .butt{color: #555555;} .butt{font-style: italic;} .butt{width: 100%}")),
 
-      fileInput("file", "Choose shape file", accept = c(".geojson",'.shp','.dbf','.sbn','.sbx','.shx', '.prj', ".cpg",'.topojson'), buttonLabel = "Browse", multiple = TRUE),
-      #textInput('url', "Insert URL", value = "", placeholder = "URL to shape file"),
-      selectInput('variables', label = 'Variables', choices = " ", multiple = TRUE),
-      selectInput('variable', label = 'Color variable', choices = " ", multiple = FALSE),
-      selectInput('palette', label = 'Palette', choices = rownames(brewer.pal.info), selected = rownames(brewer.pal.info)[1],multiple = FALSE),
-      selectInput('legend', label = 'Legend type', choices = c("cont","order", "log10",'cat','fixed','sd', "equal","pretty","quantile", "hclust", "bclust","fisher","jenks","dpih","headtails", "log10_pretty"), selected = 'pretty',multiple = FALSE),
-      selectInput('tiles', label = 'Tiles', choices = unlist(leaflet::providers), selected = 'OpenStreetMap',multiple = FALSE),
-      selectInput('pop', label = 'Popup Variables', choices = " ", multiple = TRUE),
+      fileInput("file", "Choose shape file", accept = c(".geojson", ".shp", ".dbf", ".sbn", ".sbx", ".shx", ".prj", ".cpg", ".topojson"), buttonLabel = "Browse", multiple = TRUE),
+      # textInput('url', "Insert URL", value = "", placeholder = "URL to shape file"),
+      selectInput("variables", label = "Variables", choices = " ", multiple = TRUE),
+      selectInput("variable", label = "Color variable", choices = " ", multiple = FALSE),
+      selectInput("palette", label = "Palette", choices = rownames(brewer.pal.info), selected = rownames(brewer.pal.info)[1], multiple = FALSE),
+      selectInput("legend", label = "Legend type", choices = c("cont", "order", "log10", "cat", "fixed", "sd", "equal", "pretty", "quantile", "hclust", "bclust", "fisher", "jenks", "dpih", "headtails", "log10_pretty"), selected = "pretty", multiple = FALSE),
+      selectInput("tiles", label = "Tiles", choices = unlist(leaflet::providers), selected = "OpenStreetMap", multiple = FALSE),
+      selectInput("pop", label = "Popup Variables", choices = " ", multiple = TRUE),
 
-      selectInput('download_type', label = 'Download type', choices = c('map.html', "map.png", "map.pdf", "map.jpeg", "data.geojson","data.RData","data.csv", "map&data.RData"), multiple = FALSE),
-      downloadButton('download', label = "Download", class = "butt")
-
+      selectInput("download_type", label = "Download type", choices = c("map.html", "map.png", "map.pdf", "map.jpeg", "map_static.png", "map_static.pdf", "map_static.jpeg", "data.geojson", "data.RData", "data.csv", "map&data.RData"), multiple = FALSE),
+      downloadButton("download", label = "Download", class = "butt")
     )
   ),
   dashboardBody(
@@ -48,10 +48,10 @@ ui <- dashboardPage(skin = "blue",
         "))
     ),
     fluidRow(
-      box(title = "Map", leafletOutput('map'),width = 12, collapsible = TRUE)),
+      box(title = "Map", leafletOutput("map"), width = 12, collapsible = TRUE)
+    ),
     fluidRow(
-      box(title = "Data table", dataTableOutput('table'), width = 12, collapsible = TRUE),
+      box(title = "Data table", dataTableOutput("table"), width = 12, collapsible = TRUE),
     )
-
-)
+  )
 )
