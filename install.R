8981e9af82be0b9b05e1454f082867b02a80d3ba
Alessio <srf.alessio@gmail.com>
2020-11-18 11:01:06 +0100

    Update install.R

--- install.R
+++ install.R
@@ -11,5 +11,5 @@ install.packages('tmap')
 install.packages('tmaptools')
 install.packages('RColorBrewer')
 install.packages('geojsonio', dependencies = TRUE)
-install.packages("stringr")
+install.packages("stringr", dependencies = TRUE)
 
