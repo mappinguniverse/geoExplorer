830d910e9f742560752d39da68a9db4587dbffaa
Alessio <srf.alessio@gmail.com>
2021-01-15 12:37:51 +0100

    Update shiny-server.sh

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# Make sure the directory for individual app logs exists
+# Make sure the directory for individual app logs exists.
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
 
 if [ "$APPLICATION_LOGS_TO_STDOUT" != "false" ];
 then
-	# push the "real" application logs to stdout with xtail in detached mode
+	# push the "real" application logs to stdout with xtail in detached mode.
 	exec xtail /var/log/shiny-server/ &
 fi
 
